bf1b501947cbe8b676dfc32a8f1f36d36d450ce5
PhungHuynh <huynhtrongphungse@gmail.com>
2024-10-17 22:12:06 +0700

    init project

---
+++ set_env.sh
@@ -0,0 +1,13 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=awsrds
+export POSTGRES_PASSWORD=####
+export POSTGRES_HOST=cdr-aws.cotabghsrqxz.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=arn:aws:s3:::aws-s3-cloud-dev
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:8100
+export DIALECT=postgres
+
